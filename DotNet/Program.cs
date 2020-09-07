d856eaf325d6e30e4c56913ffd2157ca46f558c6
y.bond <xisofix@gmail.com>
2020-09-07 08:11:51 +0500

    initial

---
+++ DotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
